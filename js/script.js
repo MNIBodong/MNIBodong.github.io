20afbc4b35178ef8a75f9fda1ba54ab634ae857c
MNIBodong <tk.mushlihnuri@gmail.com>
2021-07-25 22:16:04 +0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// sidenav
+const sideNav = document.querySelectorAll('.sidenav');
+M.Sidenav.init(sideNav);
+
+// slider
+const slider = document.querySelectorAll('.slider');
+M.Slider.init(slider, {
+    indicators: false,
+    height: 500,
+    transition: 600,
+    interval: 3000
+});
+
+// client
+const parallax = document.querySelectorAll('.parallax');
+M.Parallax.init(parallax);
+
+// portfolio
+const materialbox = document.querySelectorAll('.materialboxed');
+M.Materialbox.init(materialbox);
+
+// scrollspy
+const scroll = document.querySelectorAll('.scrollspy');
+M.ScrollSpy.init(scroll, {
+    scrollOffset: 50
+});
+
+// dropdown
+const dropdown = document.querySelectorAll('.dropdown-trigger');
+M.Dropdown.init(dropdown);
